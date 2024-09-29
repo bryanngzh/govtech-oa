c8602369b620426c9a935b92605820e343b72a7e
bryanngzh <bryanngzihao@gmail.com>
2024-09-30 03:09:30 +0900

    fix: update seed scripts

--- govtech-be/seed-scripts/addTeams.ts
+++ govtech-be/seed-scripts/addTeams.ts
@@ -12,18 +12,18 @@ const generateRegDate = (startDate: string, offsetMinutes: number): string => {
 const baseRegDate = "2024-09-29T08:00:00Z";
 
 const teams: Team[] = [
-  { name: "A", regDate: generateRegDate(baseRegDate, 0), group: "1" },
-  { name: "B", regDate: generateRegDate(baseRegDate, 5), group: "1" },
-  { name: "C", regDate: generateRegDate(baseRegDate, 10), group: "1" },
-  { name: "D", regDate: generateRegDate(baseRegDate, 15), group: "1" },
-  { name: "E", regDate: generateRegDate(baseRegDate, 20), group: "1" },
-  { name: "F", regDate: generateRegDate(baseRegDate, 25), group: "1" },
-  { name: "G", regDate: generateRegDate(baseRegDate, 30), group: "2" },
-  { name: "H", regDate: generateRegDate(baseRegDate, 35), group: "2" },
-  { name: "I", regDate: generateRegDate(baseRegDate, 40), group: "2" },
-  { name: "J", regDate: generateRegDate(baseRegDate, 45), group: "2" },
-  { name: "K", regDate: generateRegDate(baseRegDate, 50), group: "2" },
-  { name: "L", regDate: generateRegDate(baseRegDate, 55), group: "2" },
+  { id: "A", regDate: generateRegDate(baseRegDate, 0), group: "1" },
+  { id: "B", regDate: generateRegDate(baseRegDate, 5), group: "1" },
+  { id: "C", regDate: generateRegDate(baseRegDate, 10), group: "1" },
+  { id: "D", regDate: generateRegDate(baseRegDate, 15), group: "1" },
+  { id: "E", regDate: generateRegDate(baseRegDate, 20), group: "1" },
+  { id: "F", regDate: generateRegDate(baseRegDate, 25), group: "1" },
+  { id: "G", regDate: generateRegDate(baseRegDate, 30), group: "2" },
+  { id: "H", regDate: generateRegDate(baseRegDate, 35), group: "2" },
+  { id: "I", regDate: generateRegDate(baseRegDate, 40), group: "2" },
+  { id: "J", regDate: generateRegDate(baseRegDate, 45), group: "2" },
+  { id: "K", regDate: generateRegDate(baseRegDate, 50), group: "2" },
+  { id: "L", regDate: generateRegDate(baseRegDate, 55), group: "2" },
 ];
 
 const addTeams = async () => {
@@ -31,7 +31,7 @@ const addTeams = async () => {
     for (const team of teams) {
       const createdTeam = await teamService.createOrUpdate(team);
       console.log(
-        `Added team: ${createdTeam.name} with regDate ${createdTeam.regDate} in group ${createdTeam.group}`
+        `Added team: ${createdTeam.id} with regDate ${createdTeam.regDate} in group ${createdTeam.group}`
       );
     }
     console.log("All teams added successfully.");
