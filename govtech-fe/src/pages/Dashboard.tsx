a26238649569ec4236765639a4c89ffbc224382f
bryanngzh <bryanngzihao@gmail.com>
2024-09-29 14:21:21 +0900

    feat: add navbar skeleton

--- govtech-fe/src/pages/Dashboard.tsx
+++ govtech-fe/src/pages/Dashboard.tsx
@@ -1,19 +1,5 @@
-import { Button } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
-import { FIREBASE_AUTH } from "../configs/firebase";
-
 const Dashboard = () => {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    FIREBASE_AUTH.signOut();
-    navigate("/login");
-  };
-
-  return (
-    <div>
-      <Button onClick={handleLogout}> Log Out </Button>
-    </div>
-  );
+  return <div>HELLO</div>;
 };
 
 export default Dashboard;
