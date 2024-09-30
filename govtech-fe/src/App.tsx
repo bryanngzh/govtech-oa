0a11cdd16e382e49507d69a575f2ecf961f206fd
bryanngzh <bryanngzihao@gmail.com>
2024-09-30 14:11:07 +0900

    feat: add leaderboard route

--- govtech-fe/src/App.tsx
+++ govtech-fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import LeaderboardPage from "./pages/LeaderboardPage";
+// import MatchesPage from "./pages/MatchesPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import ProtectedRoutes from "./ProtectedRoutes";
@@ -13,7 +14,8 @@ function App() {
           <Route path="/login" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route element={<ProtectedRoutes />}>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<LeaderboardPage />} />
+            {/* <Route path="/matches" element={<MatchesPage />} /> */}
           </Route>
         </Routes>
       </Router>
