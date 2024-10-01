167cf806bee02d9d288d52121426c20256dda158
bryanngzh <bryanngzihao@gmail.com>
2024-10-01 17:24:37 +0900

    fix: add loader to auth

--- govtech-fe/src/hooks/useAuthentication.ts
+++ govtech-fe/src/hooks/useAuthentication.ts
@@ -2,12 +2,14 @@ import { onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { FIREBASE_AUTH } from "../configs/firebase";
 
-const useAuthentication = (): User | null => {
+const useAuthentication = (): { user: User | null; loading: boolean } => {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
       setUser(user);
+      setLoading(false);
     });
 
     return () => {
@@ -15,7 +17,7 @@ const useAuthentication = (): User | null => {
     };
   }, []);
 
-  return user;
+  return { user, loading };
 };
 
 export default useAuthentication;
